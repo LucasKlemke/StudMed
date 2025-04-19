88c1e09da5a3975e9db883a5a3e47c29b6eae022
Lucas Klemke <lucas.klemke@Outlook.com.br>
2025-04-19 13:15:44 -0300

    Add IF NOT EXISTS clause to password_reset_token table creation

--- lib/db/migrations/0004_whole_epoch.sql
+++ lib/db/migrations/0004_whole_epoch.sql
@@ -1,4 +1,4 @@
-CREATE TABLE "password_reset_token" (
+CREATE TABLE IF NOT EXISTS "password_reset_token" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"user_id" uuid NOT NULL,
 	"token" text NOT NULL,
